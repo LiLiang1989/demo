d772808c5976f408fe3b88dc679db4e64ad811bb
qc_12d75b7304bf464c87cbf3fdc8c0c398 <qc_12d75b7304bf464c87cbf3fdc8c0c398@git.code.tencent.com>
2019-04-13 12:56:59 +0800

    删除无用文件

---
+++ src/main/java/com/demo/config/WebMvcConfig.java
@@ -0,0 +1,19 @@
+package com.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurationSupport  {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        if(!registry.hasMappingForPattern("/static/**")){
+            registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        }
+        super.addResourceHandlers(registry);
+    }
+
+}
